a147e8b7ce8633fc8715a671377d235c18ef2e94
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-02-13 18:21:51 +0530

    adding code for extracting command details

--- connect_local_mongo.py
+++ connect_local_mongo.py
@@ -1,18 +1,14 @@
 from pymongo import MongoClient
 from config import *
+import json
+from helper import *
 
 client = MongoClient("mongodb://localhost:27017")
 
 db = client.get_database(database_name)
 # collection = db.get_collection(collection_name)
+collection_details=extract_db_details(db)
 
-collection_names = db.list_collection_names()
-
-# Iterate over each collection
-for collection_name in collection_names:
-    collection = db[collection_name]
-    count = collection.count_documents({})   
-    print(f"{collection.name} : {count}")
-
+print(json.dumps(collection_details,indent=4))
 print(db.command("dbstats"))
 client.close()
---
+++ helper.py
@@ -0,0 +1,23 @@
+def extract_db_details(db):
+    collection_names = db.list_collection_names()
+    collection_details={}
+
+    # Iterate over each collection
+    for collection_name in collection_names:
+        collection = db[collection_name]
+        # count = collection.count_documents({})   
+        # print(f"{collection.name} : {count}")
+        collection_stats= db.command("collstats", collection_name) 
+        stats_dict={}
+        stats_dict["collection_name"] = collection_stats['ns']
+        stats_dict["collection_size"] = collection_stats['size']
+        stats_dict["count"] = collection_stats['count']
+        stats_dict["storage_size"] = collection_stats['storageSize']
+        stats_dict["avg_obj_size"] = collection_stats['avgObjSize']
+        stats_dict["num_indexes"] = collection_stats['nindexes']
+        stats_dict["total_index_size"] = collection_stats['totalIndexSize']
+        stats_dict["each_index_sizes"] = collection_stats['indexSizes']
+        stats_dict["total_collection_size"] = collection_stats['totalSize']
+        collection_details[collection_name] = stats_dict
+    return collection_details
+
