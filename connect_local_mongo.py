341bc6ac9c8ab4a108d50460b82e81dca89126c4
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-02-14 15:00:52 +0530

    prior to threading

--- connect_local_mongo.py
+++ connect_local_mongo.py
@@ -1,10 +1,13 @@
 from pymongo import MongoClient
 from config import *
 from helper import *
+import json
+# from create_collections import create_collections
 
 client = MongoClient("mongodb://localhost:27017")
 
 db = client.get_database(database_name)
+# create_collections(db)
 # collection = db.get_collection(collection_name)
 
 # collections_details=extract_db_details(db)
@@ -16,7 +19,7 @@ collection_names = db.list_collection_names()
 
 for collection_name in collection_names:
     collection = db[collection_name]
-    start_upsertion(collection,collection_name,10)
+    start_upsertion(collection,collection_name)
 
 
 
---
+++ create_collections.py
@@ -0,0 +1,10 @@
+from config import *
+
+def create_collections(db):
+    num = len(db.list_collection_names())
+    print("Current no. of collections : " , num)
+    if num < TOTAL_COLLECTIONS:
+        for i in range(TOTAL_COLLECTIONS-num):
+            collection_name = f"collection_{str(uuid.uuid4())}"
+            db.create_collection(collection_name)
+
