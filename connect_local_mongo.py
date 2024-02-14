895a88e8b0f90e1db56542b183f8b2efbbfb8d8c
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-02-14 14:01:20 +0530

    addding generate document and upsert functions

--- connect_local_mongo.py
+++ connect_local_mongo.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 from config import *
-import json
 from helper import *
 
 client = MongoClient("mongodb://localhost:27017")
@@ -8,9 +7,14 @@ client = MongoClient("mongodb://localhost:27017")
 db = client.get_database(database_name)
 # collection = db.get_collection(collection_name)
 
-collections_details=extract_db_details(db)
-database_details = db.command("dbstats")
+# collections_details=extract_db_details(db)
+# database_details = db.command("dbstats")
+# print(json.dumps(collections_details,indent=4))
+# print(json.dumps(database_details,indent=4))
 
-print(json.dumps(collections_details,indent=4))
-print(json.dumps(database_details,indent=4))
+
+
+
+
+
 client.close()
