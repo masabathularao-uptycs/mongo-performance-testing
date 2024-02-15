5f888bd673bd128ec7b5cb5ff69de6780f69139f
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-02-15 23:50:28 +0530

    create indexes during collections

--- create_collections.py
+++ create_collections.py
@@ -7,7 +7,12 @@ def create_collections(db):
         for i in range(TOTAL_COLLECTIONS-num):
             print(i)
             collection_name = f"aws_{str(uuid.uuid4())}_cloudaudit"
-            db.create_collection(collection_name)
+            myColl=db.create_collection(collection_name)
+            print(collection_name)
+            myColl.create_index([('event_time', 1)])
+            myColl.create_index([('upt_table_key_value', 1), ('upt_table_key_name', 1), ('upt_table_name', 1)])
+            myColl.create_index([('event_id', 1), ('upt_table_key_value', 1), ('upt_table_key_name', 1), ('upt_table_name', 1)])
+
 
 client = connect(TAG)
 db = client.get_database(database_name)
