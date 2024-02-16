82a920bcc05931ea4b519a4acf6fa29f682c3ed0
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-02-16 13:56:39 +0530

    creating insertion rate chart

--- create_chart.py
+++ create_chart.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
-MAIN = "TEST1"
+MAIN = "TEST2"
 # Step 1: Read CSV file and extract last two columns
 df = pd.read_csv(f'{MAIN}.csv')
 x_values = df.iloc[:, -2]  # Second to last column
@@ -11,7 +11,7 @@ y_values = df.iloc[:, -1]  # Last column
 # Step 2: Create plot
 plt.figure(figsize=(10, 6))  # Adjust figure size as needed
 plt.plot(x_values, y_values)
-plt.xlabel('seconds lapsed from start of the test')  # You can customize the label
+plt.xlabel('seconds elapsed from start of the test')  # You can customize the label
 plt.ylabel('Number of documents in the database')  # You can customize the label
 plt.title('Number Of Documents VS Time')  # You can customize the title
 plt.grid(True)  # Optionally, add grid lines
@@ -19,4 +19,22 @@ plt.grid(True)  # Optionally, add grid lines
 # Format y-axis tick labels to display in full
 formatter = FuncFormatter(lambda x, _: '{:,.0f}'.format(x))
 plt.gca().yaxis.set_major_formatter(formatter)
-plt.savefig(f'{MAIN}.png', bbox_inches='tight')
+plt.savefig(f'{MAIN}_doc_vs_time.png', bbox_inches='tight')
+
+plt.close()
+
+# Calculate rate of insertion
+insertion_rate = [0]  # Start with zero rate for the first data point
+for i in range(1, len(y_values)):
+    rate = (y_values[i] -y_values[i-1]) # / (y_values[i] - y_values[i-1])
+    insertion_rate.append(rate)
+
+# Plot the rate of insertion 
+plt.figure(figsize=(10, 6))  # Adjust figure size as needed
+
+plt.plot(x_values, insertion_rate)
+plt.xlabel('Time')
+plt.ylabel('Rate of Insertion per min')
+plt.title('Rate of Insertion Over Time')
+plt.grid(True)
+plt.savefig(f'{MAIN}_rate_of_insertion.png', bbox_inches='tight')
