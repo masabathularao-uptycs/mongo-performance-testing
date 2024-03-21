975ad76fe3f5fea695222e800d9fbb421dcf3d57
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-03-21 12:48:22 +0530

    adding mongo cluster status check

--- check_mongo_status.py
+++ check_mongo_status.py
@@ -1,30 +1,42 @@
 from config import *
 
 def check_mongo_nodes_health(client):
-    healthy_nodes = []
-    unhealthy_nodes = []
+    # healthy_nodes = []
+    # unhealthy_nodes = []
 
-    mongo_nodes = secondary_nodes[:]
-    mongo_nodes.append(remote_node)
+    # mongo_nodes = secondary_nodes[:]
+    # mongo_nodes.append(remote_node)
 
-    for node in mongo_nodes:
-        try:
+    # for node in mongo_nodes:
+    #     try:
 
-            # Run serverStatus command to check node health
-            server_status = client.admin.command('serverStatus')
+    #         # Run serverStatus command to check node health
+    #         server_status = client.admin.command('serverStatus')
 
-            # Check if the node is reachable and its status
-            if server_status['ok'] == 1:
-                healthy_nodes.append(node)
-            else:
-                unhealthy_nodes.append(node)
-        except Exception as e:
-            unhealthy_nodes.append((node, str(e)))
+    #         # Check if the node is reachable and its status
+    #         if server_status['ok'] == 1:
+    #             healthy_nodes.append(node)
+    #         else:
+    #             unhealthy_nodes.append(node)
+    #     except Exception as e:
+    #         unhealthy_nodes.append((node, str(e)))
         
-    print("Healthy Nodes:")
-    for node in healthy_nodes:
-        print(node)
+    # print("Healthy Nodes:")
+    # for node in healthy_nodes:
+    #     print(node)
 
-    print("\nUnhealthy Nodes:")
-    for node, error in unhealthy_nodes:
-        print(f"{node}: {error}")
+    # print("\nUnhealthy Nodes:")
+    # for node, error in unhealthy_nodes:
+    #     print(f"{node}: {error}")
+
+    print("MONGO CLUSTER STATUS : ")
+
+    repl_status = client.admin.command('replSetGetStatus')["members"]
+    for member in repl_status:
+        print("name " , member["name"])
+        print("stateStr " , member["stateStr"])
+        print("health " , member["health"])
+        print("state " , member["state"])
+        print("------------------")
+
+
